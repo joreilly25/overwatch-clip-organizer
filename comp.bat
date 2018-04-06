1693cf6c7bb277074431f873c51e7b93d16b4144
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2018-04-05 22:44:13 -0500

    Update comp.bat

--- comp.bat
+++ comp.bat
@@ -6,7 +6,3 @@ for /f "tokens=*" %%f in ('dir /b *.mp4 2^>nul') do (
   echo "moving %%f to comp\\comp-Justin-%%f"
   move "%%f" "comp\\comp-Justin-%%f"
 )
-
-timeout /t 5 /nobreak > NUL
-GOTO startloop
-pause
