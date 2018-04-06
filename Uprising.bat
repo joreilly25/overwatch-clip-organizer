f34dea9aff784f3fa46575e976739c3e24e2b0f8
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2018-04-05 22:43:46 -0500

    Update Uprising.bat

--- Uprising.bat
+++ Uprising.bat
@@ -6,5 +6,3 @@ for /f "tokens=*" %%f in ('dir /b *.mp4 2^>nul') do (
   echo "moving %%f to Uprising\\Uprising-Justin-%%f"
   move "%%f" "Uprising\\Uprising-Justin-%%f"
 )
-
-timeout /t 5 /nobreak > NUL
