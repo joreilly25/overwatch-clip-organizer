007593efac2120269ea1b8f0cb65c9801081c1ca
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2018-04-05 22:43:59 -0500

    Update Retribution.bat

--- Retribution.bat
+++ Retribution.bat
@@ -6,7 +6,3 @@ for /f "tokens=*" %%f in ('dir /b *.mp4 2^>nul') do (
   echo "moving %%f to Retribution\\Retribution-Justin-%%f"
   move "%%f" "Retribution\\Retribution-Justin-%%f"
 )
-
-REM timeout /t 5 /nobreak > NUL
-REM GOTO startloop
-REM pause
