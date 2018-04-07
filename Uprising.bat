38cd6c25b92d67c056fa821727696f52e6d5082d
Die4Ever <30947252+Die4Ever@users.noreply.github.com>
2018-04-07 02:23:17 -0500

    Delete Uprising.bat
    removing uppercase file

--- Uprising.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-SETLOCAL ENABLEDELAYEDEXPANSION
-mkdir Uprising
-:startloop
-for /f "tokens=*" %%f in ('dir /b *.mp4 2^>nul') do (
-  echo "moving %%f to Uprising\\Uprising-Justin-%%f"
-  move "%%f" "Uprising\\Uprising-Justin-%%f"
-)
